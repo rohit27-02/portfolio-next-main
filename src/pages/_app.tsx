8dcdf09aa2f62338c68799f6af953db26ea3de86
rohit rawat <102246254+rohit27-02@users.noreply.github.com>
2022-07-13 10:57:43 +0530

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,61 @@
+import "../styles/globals.css";
+import "locomotive-scroll/dist/locomotive-scroll.css";
+
+import { LazyMotion, domAnimation } from "framer-motion";
+
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import Script from "next/script";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="/rounded-avatar.png"
+          type="image/x-icon"
+        />
+
+        <title>{`10 Bits`}</title>
+
+        <meta name="title" content="10 Bits" />
+        <meta name="description" content="10 Bits" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="10 Bits" />
+        <meta
+          property="og:description"
+          content="10 Bits"
+        />
+        <meta property="og:image" content="/avatar.jpg" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content="10 Bits" />
+        <meta
+          property="twitter:description"
+          content="10 Bits"
+        />
+        <meta property="twitter:image" content="/avatar.jpg" />
+      </Head>
+      <LazyMotion features={domAnimation}>
+        <Component {...pageProps} />
+      </LazyMotion>
+      <Script
+        strategy="lazyOnload"
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      ></Script>
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}
+      </Script>
+    </>
+  );
+}
+
+export default MyApp;
